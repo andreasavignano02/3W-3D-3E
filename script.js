6d37ff2cfbca88f7d555e7fb41231252abfc02df
andreasavignano02 <112469730+andreasavignano02@users.noreply.github.com>
2022-09-14 16:14:03 +0200

    finito esercizio

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+function idoneità(range1, range2) {
+    let utentAge = Number(prompt("quanti anni hai?", "scrivi la tua età"));
+    
+
+    if(utentAge >= range1 && utentAge <= range2) {
+        alert("Sei il benvenuto")
+    } else {
+        alert("Non puoi entrare")
+    }
+    
+}
+
+
+
